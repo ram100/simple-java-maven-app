18a8f3292137b8c999ede89764530165525d5bfa
ram100 <ramanan.sp2007@gmail.com>
2025-08-11 00:32:33 +0530

    Create HelloWorld3.java
    HelloWorld3

---
+++ HelloWorld3.java
@@ -0,0 +1,12 @@
+public class HelloWorld3 {
+
+	public HelloWorld3() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+     System.out.println("Hello World3");
+	}
+
+}
